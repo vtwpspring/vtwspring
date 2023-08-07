e0393434f6abc2dac4e421d67fd58991c1379283
alfhaos <alfhaos@naver.com>
2023-08-07 16:05:52 +0900

    정한별 2023-08-07 테스트

--- src/main/java/api/vtwspring/VtwspringApplication.java
+++ src/main/java/api/vtwspring/VtwspringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VtwspringApplication {
 
     public static void main(String[] args) {
+        System.out.println("테스트22");
         SpringApplication.run(VtwspringApplication.class, args);
     }
 
